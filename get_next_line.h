cd6c74e894e352a875206d638e85a2c798995c8b
sabax <sabanium@gmail.com>
2020-08-22 17:34:59 +0900

    norme header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fsugimot <fsugimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 07:55:30 by fsugimot          #+#    #+#             */
-/*   Updated: 2020/07/20 20:58:25 by fsugimot         ###   ########.fr       */
+/*   Updated: 2020/08/22 17:32:19 by fsugimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
 
-/*int		ft_strlen(const char *dst);
-void	ft_memcpy(void *dst, void *src, size_t size);
-void	*reallocate(char *prev, int add_size, int opt);
-int	    get_end_of_line(char *line);*/
+int		ft_strlen(const char *dst);
+int		is_parsable(char *line, int ind);
+char	*add_memback(char *line, int size);
+int		fetch_line(int fd, char **line, int ini_i);
+char	*cut_back(char *line, int front, int back);
+void	cut_front(char *src, char **dst, int back);
+int		cut_line(char **src, char **dst, int reari);
 int	    get_next_line(int fd, char **line);
 
 #endif
