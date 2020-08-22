f16db6e7f684d971602a90c8270a431f5008e0d7
sabax <sabanium@gmail.com>
2020-08-22 17:45:37 +0900

    norme headerfile

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fsugimot <fsugimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 07:55:30 by fsugimot          #+#    #+#             */
-/*   Updated: 2020/08/22 17:42:13 by fsugimot         ###   ########.fr       */
+/*   Updated: 2020/08/22 17:45:32 by fsugimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int		fetch_line(int fd, char **line, int ini_i);
 char	*cut_back(char *line, int front, int back);
 void	cut_front(char *src, char **dst, int back);
 int		cut_line(char **src, char **dst, int reari);
-int     get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
 
 #endif
